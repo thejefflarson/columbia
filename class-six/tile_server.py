bcea65db5fe51993e718eddf92e32f9cc9fa2eb8
Jeff Larson <thejefflarson@gmail.com>
2016-10-21 06:39:09 -0400

    seven

--- class-six/tile_server.py
+++ class-six/tile_server.py
@@ -2,15 +2,18 @@ from math import atan, sinh, pi as π
 
 import fiona
 from flask import Flask
-from shapely import MultiPoint, mapping, shape
-app = Flask()
+import json
+from shapely.geometry import MultiPoint, Point, mapping, shape
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
 # from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 def tile2ll(x, y, z):
     n = 2 ** z
-    lon = float(x) / n * 360.0 - 180.0
-    lat = atanh(sinh(π * (1 - 2 * y / n))) * 180.0 / π
+    lon = x / n * 360.0 - 180.0
+    lat = atan(sinh(π * (1 - 2 * y / n))) * 180.0 / π
     return [lon, lat]
 
 
@@ -19,13 +22,32 @@ def tile(x=None, y=None, z=None):
     if not (x and y and z):
         raise Exception('Need x, y, and z got: %s, %s, %s' % (x, y, z))
     with fiona.drivers():
-        with fiona.open('world.json') as source:
+        with fiona.open('world.shp') as source:
+            x, y, z = [float(x), float(y), float(z)]
             minx, miny = tile2ll(x, y, z)
             maxx, maxy = tile2ll(x + 1, y + 1, z)
-            for s in source.items(bbox=(minx, miny, maxx, maxy)):
+            bbox = MultiPoint([
+                Point(minx, miny),
+                Point(minx, maxy),
+                Point(maxx, maxy),
+                Point(maxx, miny)
+            ]).envelope
+            print(minx, miny, maxx, maxy)
+            features = []
+            for _, s in source.items(bbox=(minx, miny, maxx, maxy)):
+                geom = shape(s['geometry'])
+                if geom.intersects(bbox):
+                    intsx = geom.intersection(bbox)
+                    features.append({
+                        'geometry': mapping(intsx),
+                        'properties': s['properties'],
+                        'type': 'Feature'
+                    })
 
+            return json.dumps({'land':{
+                'type': 'FeatureCollection',
+                'features': features
+            }})
 
-
-
-if __name__ == "main":
+if __name__ == "__main__":
     app.run()
