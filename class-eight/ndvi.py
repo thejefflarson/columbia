65896d83fbfb12744e9c7e02eec04f023f5fe548
Jeff Larson <thejefflarson@gmail.com>
2016-11-04 09:06:15 -0400

    class eight

--- class-eight/ndvi.py
+++ class-eight/ndvi.py
@@ -5,23 +5,14 @@ import rasterio as rio
 
 bands = []
 for i in [5, 4]:
-    with rio.open('nyc/B%s.TIF' % i) as raster:
-        bands.append(raster.read(1))
+    with rio.open('brazil/B%s.TIF' % i) as raster:
+        bands.append(raster.read(1).astype(np.float32))
         profile = raster.meta
 
 nir, r = bands
-ndvi = (nir - r) / float(nir + r) + 1
+ndvi = (nir - r) / (nir + r)
 
-
-def correct(img):
-    mask = np.logical_and(img > 0, img < 65535)
-    top = np.percentile(img[mask], 99.9)
-    bottom = np.percentile(img[mask], 0.01)
-    scaled = (img - bottom) / float(top - bottom) * 65535
-    return np.clip(scaled, 0, 65535).astype(np.uint16)
-
-img = correct(np.array(ndvi))
-profile['count'] = img.shape[0]
-profile['photometric'] = 'RGB'
-with rio.open('nyc/ndvi.tif', 'w', **profile) as dest:
-    dest.write(correct(img), indexes=list(range(img.shape[0] + 1))[1:])
+profile['dtype'] = 'float32'
+profile['nodata'] = 0
+with rio.open('brazil/ndvi.tif', 'w', **profile) as dest:
+    dest.write(ndvi, 1)
