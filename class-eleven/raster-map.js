2f5f13d97cc813626a855ca226951d103b3a903f
Jeff Larson <thejefflarson@gmail.com>
2016-12-02 07:00:39 -0500

    stuff

--- class-eleven/raster-map.js
+++ class-eleven/raster-map.js
@@ -1,23 +1,37 @@
 d3.json('durango.json', function(error, data) {
-  if(error) throw error;
+  d3.json('durango-contours.json', function(error, contours) {
+    if(error) throw error;
 
-  var durango = d3.select("#durango");
+    var durango = d3.select("#durango");
 
-  durango.style("width", 401)
-         .style("height", 623);
+    durango.style("width", 401)
+      .style("height", 623);
 
-  var path = d3.geoPath()
-               .projection(d3.geoMercator()
-                             .fitSize([401, 623], data));
+    var path = d3.geoPath()
+          .projection(d3.geoMercator()
+                      .fitSize([401, 623], data));
 
-  durango.append('image')
-    .attr("href", "durango.png")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", 401)
-    .attr("height", 623);
+    durango.append('image')
+      .attr("href", "durango.png")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", 401)
+      .attr("height", 623);
 
-  durango.append("path")
-    .datum(data)
-    .attr("d", path);
+
+    durango.append('path')
+      .datum(contours)
+      .attr('class', 'contours')
+      .attr('d', path);
+
+    durango.selectAll("path")
+      .data(data.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", function(it) {
+        if(it.properties.MTFCC == "S1400") return "small";
+        return "big";
+      });
+  });
 });
